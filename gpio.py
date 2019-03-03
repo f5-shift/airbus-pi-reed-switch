aeed91830ec978605f47db6ffd0a8a7dc49b2022
Pi <pi@pi.com>
2019-03-03 16:11:41 +0000

    Added support for busy polling during announcement period

--- gpio.py
+++ gpio.py
@@ -11,6 +11,8 @@ URL = "https://sheltered-citadel-43963.herokuapp.com"
 TIMEOUT = 5
 
 seat = '6A'
+announcement = False
+cached = None
 pp = PrettyPrinter(indent=4)
 
 def export(pin):
@@ -37,20 +39,60 @@ def setup():
 
 
 def onchange(gpio):
+  global announcement
+  if announcement:
+    print("annoucement, so ignore transition")
+    return
+
   params = {'seat': seat }
+  res = None
   if GPIO.input(REED_PIN):  
     # high signal := open
     print("Rising edge = unbuckled")
-    requests.get(url="{}/unbuckle".format(URL), params=params)
+    res = requests.get(url="{}/unbuckle".format(URL), params=params)
   else:
     # low signal := closed
     print("Falling edge = buckled")
-    requests.get(url="{}/buckle".format(URL), params=params)
+    res = requests.get(url="{}/buckle".format(URL), params=params)
+
+  if res.status_code == 503 and not announcement:
+    announcement = True
+
+    # busy poll for announcement to stop
+    # then send updated state
+    while True:
+      state = GPIO.input(REED_PIN)
+      buckled = 1 - state
+
+      print("announcement, sending %d" % buckled)
+      res = requests.get(url="{}/init".format(URL), params={ 'seat': seat, 'buckled':  buckled })
+
+      cached = buckled
+      if res.status_code == 200:
+        print("announcement stopped")
+        break
+    
+      sleep(0.5)
+
+    if cached is not None:
+      requests.get(url="{}/init".format(URL), params={ 'seat': seat, 'buckled':  cached })
+
+    # reset cache and announcement flag
+    cached = None
+    announcement = False
 
 
 def main():
   # onchange callback
   GPIO.add_event_detect(REED_PIN, GPIO.BOTH, callback=onchange, bouncetime=10)
+
+  # send initial state to server
+  state = GPIO.input(REED_PIN)
+  buckled = 1 - state
+  res = requests.get(url="{}/init".format(URL), params={ 'seat': seat, 'buckled':  buckled })
+
+  # TODO deal with announcement status code
+  print("Status code", res.status_code)
 
   print("Initial state of", REED_PIN, "is", GPIO.input(REED_PIN))
   
